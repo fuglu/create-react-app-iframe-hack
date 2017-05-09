db2595e8a93606c5efc73a7583748670a4cfb342
Benjamin Kluck <ben@fug.lu>
2017-05-10 00:00:32 +0200

    iframe hack proof of concept
    Ugly as fuuu. May eat your cat.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,24 @@ import ReactDOMServer from 'react-dom/server';
 import App from './App';
 import './index.css';
 
+/*
+ * TODO
+ * Only if process.env.NODE_ENV === 'development'
+ * Do something else in production
+*/
+const copyCssFromParent = `
+    var styles = parent.document.getElementsByTagName("style");
+    for(var i = 0; i < styles.length; i++)
+    {
+      document.head.appendChild(styles[i]);
+    }
+`;
+
 const html = ReactDOMServer.renderToString(
 	<html>
 		<head>
 			<title>Test</title>
+			<script dangerouslySetInnerHTML={{__html: copyCssFromParent}} />
 		</head>
 		<body>
 			<App />
